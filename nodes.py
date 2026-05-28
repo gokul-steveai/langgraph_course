4fa6a0c915441d67fe07739e8ff0140c70eec5d1
Gokul Panwar <gokul.stevesai@gmail.com>
2026-05-28 14:07:41 +0530

    created graph

--- nodes.py
+++ nodes.py
@@ -1,26 +1,24 @@
 from dotenv import load_dotenv
 from langgraph.graph import MessagesState
 from langgraph.prebuilt import ToolNode
+
 from react import llm, tools
 
 load_dotenv()
 
-SYSTEM_MESSAGE = """
-You are a helpful assistant that can answer the question using the tools provided.
+SYSYEM_MESSAGE = """
+You are a helpful assistant that can use tools to answer questions.
 """
 
 
 def run_agent_reasoning(state: MessagesState) -> MessagesState:
     """
-    Run the agent reasoning loop.
+    Run the agent reasoning node.
     """
     response = llm.invoke(
-        {"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]
+        [{"role": "system", "content": SYSYEM_MESSAGE}, *state["messages"]]
     )
-
-    return {
-        "messages": [response],
-    }
+    return {"messages": [response]}
 
 
 tool_node = ToolNode(tools)
--- react.py
+++ react.py
@@ -11,29 +11,12 @@ load_dotenv()
 @tool
 def triple(num: float) -> float:
     """
-    Triple a number.
-
-    Args:
-        num (float): The raw float or integer number to triple. Do NOT wrap this in string quotes.
-
-    Returns:
-        float: The triple of the input number.
+    param num: a number to triple
+    returns: the triple of the input number
     """
     return float(num) * 3
 
 
-tools = [TavilySearch(max_results=1), triple]
-
+tools = [TavilySearch(max_results=1, topic="general"), triple]
 
 llm = ChatGroq(model=os.environ["MODEL_NAME"], temperature=0).bind_tools(tools)
-
-
-def main():
-    user_prompt = "What is the triple value of 3?"
-    response = llm.invoke(user_prompt)
-
-    print(response)
-
-
-if __name__ == "__main__":
-    main()
